01ee0b0d4b45f9f0f395871801f4c4fc6afe844b
Kamil S <velerad007@gmail.com>
2022-12-03 22:32:46 +0100

    adding day 3

--- Package.swift
+++ Package.swift
@@ -3,20 +3,25 @@
 
 import PackageDescription
 
+let resources: [Resource] = [.process("Resources")]
+
 let day1 = "Day1"
 let day2 = "Day2"
+let day3 = "Day3"
 
 let package = Package(
     name: "Advent of code",
     products: [
       .executable(name: day1, targets: [day1]),
       .executable(name: day2, targets: [day2]),
+      .executable(name: day3, targets: [day3]),
     ],
     dependencies: [
 
     ],
     targets: [
-      .target(name: day1, resources: [.process("Resources")]),
-      .target(name: day2, resources: [.process("Resources")]),
+      .executableTarget(name: day1, resources: resources),
+      .executableTarget(name: day2, resources: resources),
+      .executableTarget(name: day3, resources: resources),
     ]
 )
---
+++ Sources/Day3/main.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+guard let inputUrl = Bundle.module.url(forResource: "input", withExtension: "txt") else {
+  fatalError("Input resource not found")
+}
+
+guard let input = try? String.init(contentsOf: inputUrl) else {
+  fatalError("Input is not a string")
+}
+
+
